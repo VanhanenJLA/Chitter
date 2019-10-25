4cb11de686a983541e85360b7e030488c63cb17c
VanhanenJLA <jouni.vanhanen@outlook.com>
2019-10-25 10:22:53 +0300

    Add BS4 Floating labels login

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ export class App {
         config.map([
 
             { route: '', moduleId: PLATFORM.moduleName('./pages/reader/reader'), title: "Reader", nav: true },
+            { route: 'login', name: 'login', moduleId: PLATFORM.moduleName('./pages/login/login'), title: "Login", nav: true },
             { route: 'greet', moduleId: PLATFORM.moduleName('./pages/greeter/greeter'), title: "Greeter", nav: true },
             { route: 'hello', name: 'hello', moduleId: PLATFORM.moduleName('./pages/hello/hello'), title: "Hello", nav: true },
 
