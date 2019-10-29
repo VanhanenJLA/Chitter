3ba6538d10f657e9ad64a1bc8e15c81bd9b63dc2
VanhanenJLA <jouni.vanhanen@outlook.com>
2019-10-29 10:49:18 +0200

    Add components to reader page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,16 +4,24 @@ import { LoginService } from "./login-service";
 @inject(Aurelia)
 export class Login {
 
+    _rememberMe = true;
+
     constructor(Aurelia, LoginService) {
         this.aurelia = Aurelia;
         this.loginService = LoginService;
     }
 
     login(username, password) {
-        alert(`${username} ${password}`);
+        // alert(`${username} ${password}`);
         // this.loginService.Login(username, password);
         if (username && password)
             this.aurelia.setRoot(PLATFORM.moduleName('app'));
+    }
+
+    attached() {
+        if (this._rememberMe) {
+            this.login("admin", "admin");
+        }
     }
 
 }
