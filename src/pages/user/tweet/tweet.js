e0f6238f0d42ad5760ab5ff0f7fa022a8f8283d6
VanhanenJLA <jouni.vanhanen@outlook.com>
2019-12-16 13:04:36 +0200

    Update tweet

--- src/pages/user/tweet/tweet.js
+++ src/pages/user/tweet/tweet.js
@@ -1,10 +1,19 @@
+import { bindable } from "aurelia-framework";
+
 export class Tweet {
 
+    @bindable author;
+
     constructor() {
-        let str = "This is content btw. ";
-        for (let i = 0; i < 3; ++i) {
-            str = str.concat(str);
-        }
-        this.content = str;
+        this.content = (() => {
+            let str = "This is content btw. ";
+            for (let i = 0; i < 3; ++i) str = str.concat(str);
+            return str;
+        })();
     }
+
+    createContent() {
+
+    }
+    
 }
