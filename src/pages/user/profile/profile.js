a0d193378ef57c33a3ee43a3d0a0ae99baa43c2f
VanhanenJLA <jouni.vanhanen@outlook.com>
2019-12-16 13:03:32 +0200

    Remove

--- src/pages/user/profile/profile.js
+++
@@ -1,8 +0,0 @@
-export class Profile {
-    constructor() {
-        this.bio = "Bio goes here. ";
-        for (let i = 0; i < 3; ++i) {
-            this.bio = this.bio.concat(this.bio);
-        }
-    }
-}
