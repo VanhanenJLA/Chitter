d8deec1d27a6621c67763e4cad74daa3f8dd59a9
VanhanenJLA <jouni.vanhanen@outlook.com>
2020-01-03 15:13:22 +0200

    Simplify user-page
    + Autofixed ESLint nags

--- src/pages/user/tweet/tweet.js
+++ src/pages/user/tweet/tweet.js
@@ -1,19 +1,13 @@
-import { bindable } from "aurelia-framework";
+import { bindable } from 'aurelia-framework';
 
 export class Tweet {
-
     @bindable author;
 
     constructor() {
-        this.content = (() => {
-            let str = "This is content btw. ";
-            for (let i = 0; i < 3; ++i) str = str.concat(str);
-            return str;
-        })();
+      this.content = (() => {
+        let str = 'This is content btw. ';
+        for (let i = 0; i < 3; ++i) str = str.concat(str);
+        return str;
+      })();
     }
-
-    createContent() {
-
-    }
-    
 }
--- src/pages/user/user-service.js
+++ src/pages/user/user-service.js
@@ -1,4 +1,4 @@
-
+import ProfilePicture from '../../static/profile.png';
 export class UserService {
   createUser() {
     let firstname = 'Jouni';
@@ -9,16 +9,24 @@ export class UserService {
       for (let i = 0; i < 5; i++) s += 'This is a bio btw. ';
       return s;
     })();
-    return new UserDTO(firstname, lastname, handle, bio);
+    let profilePicture = ProfilePicture;
+    let pictures = (() => {
+      let _pictures = [];
+      for (let i = 0; i < 6; i++) _pictures[i] = profilePicture;
+      return _pictures;
+    })();
+    return new UserDTO(firstname, lastname, handle, bio, profilePicture, pictures);
   }
 }
 
 class UserDTO {
-  constructor(firstname, lastname, handle, bio) {
+  constructor(firstname, lastname, handle, bio, profilePicture, pictures) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.fullname = `${firstname} ${lastname}`;
     this.handle = handle;
     this.bio = bio;
+    this.profilePicture = profilePicture;
+    this.pictures = pictures;
   }
 }
