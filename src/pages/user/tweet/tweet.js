bb03d2224afea87df96007ec447b58abdbd1a3db
Jouni Vanhanen <jouni.vanhanen@outlook.com>
2020-01-22 00:02:58 +0200

    Add WIP comment feature

--- src/pages/user/tweet/tweet.js
+++ src/pages/user/tweet/tweet.js
@@ -1,14 +1,28 @@
 import { bindable } from 'aurelia-framework';
 
 export class Tweet {
-  @bindable author;
-  @bindable comments
+    @bindable author;
+    @bindable comments
 
-  constructor() {
-    this.content = (() => {
-      let str = 'This is content btw. ';
-      for (let i = 0; i < 3; ++i) str = str.concat(str);
-      return str;
-    })();
-  }
+    constructor() {
+        this.retweet = null;
+        this.commenting = false;
+        this.content = (() => {
+            let str = 'This is content btw. ';
+            for (let i = 0; i < 3; ++i) str = str.concat(str);
+            return str;
+        })();
+    }
+
+    commentClicked() {
+        console.log("Comment clicked!");
+        this.commenting = true;
+    }
+
+    retweetClicked(event, data) {
+        console.log("Retweet clicked!");
+        console.log(event);
+        console.log(data);
+        this.retweet = data;
+    }
 }
