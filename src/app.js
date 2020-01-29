ec49adff2c66e61c62ab4b3e1b4f6e5bcc860134
Jouni Vanhanen <jouni.vanhanen@outlook.com>
2020-01-29 21:37:40 +0200

    Add theming

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
-import { PLATFORM } from 'aurelia-framework';
+import { PLATFORM, bindable } from 'aurelia-framework';
 
 export class App {
+
+    @bindable selectedTheme;
     constructor() {
+
+        this.themes = [
+            {
+                title: "default",
+                url:
+                    "https://bootswatch.com/_vendor/bootstrap/dist/css/bootstrap.min.css"
+            },
+            {
+                title: "cerulean",
+                url: "https://bootswatch.com/4/cerulean/bootstrap.min.css"
+            },
+            {
+                title: 'darkly',
+                url: 'https://bootswatch.com/4/darkly/bootstrap.min.css'
+            }
+        ];
 
     }
 
@@ -12,29 +30,37 @@ export class App {
 
             { route: '', moduleId: PLATFORM.moduleName('./pages/user/user-page'), title: 'User', nav: false },
             { route: 'user', moduleId: PLATFORM.moduleName('./pages/user/user-page'), title: 'User', nav: true },
-            { route: 'vr', moduleId: PLATFORM.moduleName('./pages/vr/vr-page'), title: 'VR', nav: true },
             { route: 'svg', moduleId: PLATFORM.moduleName('./pages/svg/svg-page'), title: 'SVG', nav: true },
+            // { route: 'vr', moduleId: PLATFORM.moduleName('./pages/vr/vr-page'), title: 'VR', nav: true },
 
         ]);
     }
 
     attached() {
 
-        const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-        function switchTheme(e) {
-            let theme = e.target.checked ? 'dark' : 'light';
-            document.documentElement.setAttribute('data-theme', theme);
-            localStorage.setItem('theme', theme);
-        }
-        toggleSwitch.addEventListener('change', switchTheme, false);
-
         const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
         if (currentTheme) {
             document.documentElement.setAttribute('data-theme', currentTheme);
+        }
+    }
 
-            if (currentTheme === 'dark') {
-                toggleSwitch.checked = true;
-            }
+    activate() {
+        // this.changeTheme(this.themes[0]);
+    }
+    changeTheme(theme) {
+        const head = document.getElementsByTagName("head")[0];
+        const itemId = 'css-sheet';
+        let link = document.getElementById(itemId);
+        if (!link) {
+            link = document.createElement("link");
+            link.id = itemId;
+            link.rel = "stylesheet";
+            link.type = "text/css";
+            link.href = theme.url;
+            link.media = "all";
+            head.appendChild(link);
+        } else {
+            link.href = theme.url;
         }
     }
 
---
+++ src/resources/elements/theme-select.js
@@ -0,0 +1,112 @@
+
+export class ThemeSelect {
+
+    themes = [
+        {
+            title: "Default",
+            url:
+                "./../../static/bootstrap.min.css"
+        },
+        {
+            title: "Cerulean",
+            url: "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/cerulean/bootstrap.min.css"
+        },
+        {
+            title: 'Cosmo',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/cosmo/bootstrap.min.css'
+        },
+        {
+            title: 'Cyborg',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/cyborg/bootstrap.min.css'
+        },
+        {
+            title: 'Darkly',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/darkly/bootstrap.min.css'
+        },
+        {
+            title: 'Flatly',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/flatly/bootstrap.min.css'
+        },
+        {
+            title: 'Journal',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/journal/bootstrap.min.css'
+        },
+        {
+            title: 'Litera',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/litera/bootstrap.min.css'
+        },
+        {
+            title: 'Lumen',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/lumen/bootstrap.min.css'
+        },
+        {
+            title: 'Lux',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/lux/bootstrap.min.css'
+        },
+        {
+            title: 'Materia',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/materia/bootstrap.min.css'
+        },
+        {
+            title: 'Minty',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/minty/bootstrap.min.css'
+        },
+        {
+            title: 'Pulse',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/pulse/bootstrap.min.css'
+        },
+        {
+            title: 'Sandstone',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/sandstone/bootstrap.min.css'
+        },
+        {
+            title: 'Simplex',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/simplex/bootstrap.min.css'
+        },
+        {
+            title: 'Sketchy',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/sketchy/bootstrap.min.css'
+        },
+        {
+            title: 'Slate',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/slate/bootstrap.min.css'
+        },
+        {
+            title: 'Solar',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/solar/bootstrap.min.css'
+        },
+        {
+            title: 'Spacelab',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/spacelab/bootstrap.min.css'
+        },
+        {
+            title: 'Superhero',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/superhero/bootstrap.min.css'
+        },
+        {
+            title: 'United',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/united/bootstrap.min.css'
+        },
+        {
+            title: 'Yeti',
+            url: 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.4.1/yeti/bootstrap.min.css'
+        }
+    ];
+
+    changeTheme(theme) {
+        const head = document.getElementsByTagName("head")[0];
+        const itemId = 'css-sheet';
+        let link = document.getElementById(itemId);
+        if (!link) {
+            link = document.createElement("link");
+            link.id = itemId;
+            link.rel = "stylesheet";
+            link.type = "text/css";
+            link.href = theme.url;
+            link.media = "all";
+            head.appendChild(link);
+        } else {
+            link.href = theme.url;
+        }
+    }
+}
