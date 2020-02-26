71195db82f4abdd9628fafe52a9153d74c6e6c2f
Jouni Vanhanen <jouni.vanhanen@outlook.com>
2020-02-26 21:51:15 +0200

    Simulate new incoming tweets

--- src/pages/user/user-page.js
+++ src/pages/user/user-page.js
@@ -5,18 +5,28 @@ import { UserService } from '../user/user-service'
 export class UserPage {
 
     @bindable user;
-    @bindable retweet;
+    tweets;
 
-    constructor(UserService) {
-        this.userService = UserService;
+    constructor(userService) {
+        this.userService = userService;
         this.user = this.userService.createUser();
         let comment = this.userService.createComment(this.user, "This is a comment btw.");
-        this.comments = [comment, comment, comment, comment];
+        let comments = [comment, comment, comment, comment];
 
         let tweet = this.userService.createTweet(this.user,
-            "This is content btw. This is content btw. This is content btw. ", this.comments);
+            "This is content btw. This is content btw. This is content btw. ", comments);
 
         this.tweets = [tweet, tweet];
+    }
+
+    attached() {
+        setInterval(() => {
+            let comment = this.userService.createComment(this.user, "This is a comment btw.");
+            let comments = [comment, comment, comment, comment];
+            this.tweets
+                .push(this.userService.createTweet(this.user,
+                    "This is content btw. This is content btw. This is content btw. ", comments))
+        }, 5000);
     }
 
 }
