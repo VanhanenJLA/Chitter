f5bc6f747d6a83f8b9b7c4365bd7a6efbed32554
Jouni Vanhanen <jouni.vanhanen@outlook.com>
2020-03-01 23:26:51 +0200

    Change world into an svg custom element
    Also hide jumbotron on svg-page navigation

--- src/pages/svg/svg-page.js
+++ src/pages/svg/svg-page.js
@@ -1,17 +1,9 @@
 import svgPanZoom from 'svg-pan-zoom';
-import world_map from './../../static/svg/world_map.svg';
-import world from './../../static/svg/world.svg';
 
 export class SvgPage {
-    constructor() {
-        this.world = world;
-        this.world_map = world_map;
-    }
 
-    attached() {
-        this.zoomable.addEventListener('load', function() {
-            svgPanZoom(this);
-        });
-
-    }
+  attached() {
+    document.getElementById('jumbotron').hidden = "true";
+    svgPanZoom(document.getElementById('svg'));
+  }
 }
