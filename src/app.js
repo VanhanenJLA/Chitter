d94a50c972544c3f5b98171e7906e91a60301ec7
Jouni Vanhanen <jouni.vanhanen@outlook.com>
2020-07-13 00:09:01 +0300

    Fix indentation

--- src/app.js
+++ src/app.js
@@ -4,23 +4,21 @@ import { SharedState } from './shared/shared-state';
 @inject(Aurelia, SharedState)
 export class App {
 
-    constructor(aurelia, sharedState) {
-        this.aurelia = aurelia;
-        this.sharedState = sharedState;
-    }
+  constructor(aurelia, sharedState) {
+    this.aurelia = aurelia;
+    this.sharedState = sharedState;
+  }
 
-    configureRouter(config, router) {
-        this.router = router;
-        config.title = 'Chitter';
-        config.map([
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Chitter';
+    config.map([
+      { route: ['', 'user'], moduleId: PLATFORM.moduleName('./pages/user/user-page'), title: 'User', nav: true },
+    ]);
+  }
 
-            { route: ['', 'user'], moduleId: PLATFORM.moduleName('./pages/user/user-page'), title: 'User', nav: true },
-
-        ]);
-    }
-
-    logout() {
-        this.aurelia.setRoot(PLATFORM.moduleName('pages/login/login-page'));
-    }
+  logout() {
+    this.aurelia.setRoot(PLATFORM.moduleName('pages/login/login-page'));
+  }
 
 }
